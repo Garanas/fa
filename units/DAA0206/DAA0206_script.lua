152410c015cebab2198aa86e6338fa30342e9176
baurse <18741277+baurse@users.noreply.github.com>
2021-08-06 00:05:42 +0200

    Very slightly optimized DAA0206, the mercy

--- units/DAA0206/DAA0206_script.lua
+++ units/DAA0206/DAA0206_script.lua
@@ -5,7 +5,7 @@
 #**
 #**  Summary  :  Aeon Guided Missile Script
 #**
-#**  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+#**  Copyright ï¿½ 2007 Gas Powered Games, Inc.  All rights reserved.
 #****************************************************************************
 local AAirUnit = import('/lua/aeonunits.lua').AAirUnit
 local DefaultProjectileWeapon = import('/lua/sim/defaultweapons.lua').DefaultProjectileWeapon
@@ -16,16 +16,16 @@ DAA0206 = Class(AAirUnit) {
     Weapons = {
         Suicide = Class(DefaultProjectileWeapon) {}
     },
-    
+
     OnRunOutOfFuel = function(self)
-        self:Kill()
+        self.Kill(self)
     end,
     
     ProjectileFired = function(self)
-        self:GetWeapon(1).IdleState.Main = function(self) end
-        self:PlayUnitSound('Killed')
-		self:PlayUnitSound('Destroyed')
-        self:Destroy()  			
+        self.GetWeapon(self, 1).IdleState.Main = function(self) end
+        self.PlayUnitSound(self, 'Killed')
+		self.PlayUnitSound(self, 'Destroyed')
+        self.Destroy(self)
     end,
 }
 TypeClass = DAA0206
